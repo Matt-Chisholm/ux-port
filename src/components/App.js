b63678c8b2ceb2863efc60e93cbd608988f82c3c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-31 11:59:31 -0800

    bg play

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className='bg-image'>
+      <div className='main-card'>
+        <h1 className='title'>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
