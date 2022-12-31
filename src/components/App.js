3807f2df8dfea6b830d5f0358ee87ecdce1118bc
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-12-31 14:51:46 -0800

    starting context

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import "../styles/App.css";
 import Nav from "./Nav";
+import Contact from "./Contact";
+import About from "./About";
+import Projects from "./Projects";
+import Home from "./Home";
 
 function App() {
   return (
     <div className='main-container'>
       <div className='main-card'>
         <Nav />
-        <h1 className='main-title'>Matt Chisholm</h1>
       </div>
     </div>
   );
