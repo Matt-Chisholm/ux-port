045dd13a694fa8ab887820960a6a11ca38824529
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-04 12:19:04 -0800

    contact shenanigans

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,45 @@
 import React from "react";
+import "../styles/Contact.css";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  const [formStatus, setFormStatus] = React.useState("Send");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormStatus("Submitting...");
+    const { name, email, message } = e.target.elements;
+    let conFom = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    console.log(conFom);
+  };
+  return (
+    <div className='container mt-5'>
+      <h1 className='mb-4'>Send me an Email</h1>
+      <form onSubmit={onSubmit}>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='name'>
+            Name
+          </label>
+          <input className='form-control' type='text' id='name' required />
+        </div>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='email'>
+            Email
+          </label>
+          <input className='form-control' type='email' id='email' required />
+        </div>
+        <div className='mb-3'>
+          <label className='form-label' htmlFor='message'>
+            Message
+          </label>
+          <textarea className='form-control' id='message' required />
+        </div>
+        <button className='form-btn' type='submit'>
+          {formStatus}
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
