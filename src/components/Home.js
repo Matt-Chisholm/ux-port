2cac65c0a5a9f18a97f908254c15c0467d5e24e0
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-04 11:15:50 -0800

    so much styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,12 +23,36 @@ export default function Home() {
   return (
     <div className='home-container'>
       <div className='text-container'>
-        <h1 className='name-h1'>
-          Hi, I'm <span className='name'>Matt</span>.
-        </h1>
-        <h2 className='scroller'>
-          I'm <span className='job'>{word}</span>.
-        </h2>
+        <div className='name-container'>
+          <h1 className='name-h1'>
+            Hi, I'm <span className='name'>Matt</span>.
+          </h1>
+          <img
+            className='logo'
+            alt=''
+            src='https://img.icons8.com/clouds/200/000000/react.png'
+          />
+          <img
+            className='logo'
+            alt=''
+            src='https://img.icons8.com/fluency/200/000000/typescript--v1.png'
+          />
+          <img
+            className='logo'
+            alt=''
+            src='https://img.icons8.com/fluency/200/000000/javascript.png'
+          />
+          <img
+            className='logo'
+            alt=''
+            src='https://img.icons8.com/color/200/000000/nodejs.png'
+          />
+        </div>
+        <div className='carousel-container'>
+          <h2 className='scroller'>
+            I'm <span className='job'>{word}</span>.
+          </h2>
+        </div>
       </div>
     </div>
   );
