609fc41648e8018197db63d081d4d3b17b1815aa
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-04 10:37:52 -0800

    word carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  const words = [
+    "web developer",
+    "front-end developer",
+    "software engineer",
+    "nerd",
+  ];
+  const [word, setWord] = React.useState(words[0]);
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setWord(words[index]);
+      setIndex((index + 1) % words.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [index]);
+
+  return (
+    <div className='home-container'>
+      <div className='home-text'>
+        <h1>
+          Hi, I'm <span className='name'>Matt</span>.
+        </h1>
+        <h2>
+          I'm a <span className='job'>{word}</span>.
+        </h2>
+      </div>
+    </div>
+  );
 }
