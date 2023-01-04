6e2c0cb25c25f151fb0e20e09ce1926b3f520dbd
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-04 15:03:30 -0800

    fade ins

--- src/components/About.js
+++ src/components/About.js
@@ -7,24 +7,26 @@ export default function About() {
       <div className='about-card'>
         <div className='about-text'>
           <h1 className='about-h1'>About Me</h1>
-          <p className='about-p'>
-            I am a full-stack web developer with a current focus on front-end.
-            <br />
-            I am currently working mostly in React, React Native and learning
-            the art of Typescript.
-            <br />
-            I am as of now involved in several open-source and volunteer
-            projects, but am always looking for more.
-            <br />
-            I am a graduate of Lighthouse Labs Web Development Bootcamp, and am
-            constantly upskilling.
-            <br />
-            Although I am currently focused on honing my front-end skills, I
-            also enjoy getting involved in design and back-end development..
-            <br />
-            I am a lifelong learner, and am always looking for new challenges.
-            <br />
-          </p>
+          <div className='color-bg'>
+            <p className='about-p'>
+              I am a full-stack web developer with a current focus on front-end.
+              <br />
+              I am currently working mostly in React, React Native and learning
+              the art of Typescript.
+              <br />
+              I am as of now involved in several open-source and volunteer
+              projects, but am always looking for more.
+              <br />
+              I am a graduate of Lighthouse Labs Web Development Bootcamp, and
+              am constantly upskilling.
+              <br />
+              Although I am currently focused on honing my front-end skills, I
+              also enjoy getting involved in design and back-end development..
+              <br />
+              I am a lifelong learner, and am always looking for new challenges.
+              <br />
+            </p>
+          </div>
         </div>
         <div className='btn-container'>
           <a
