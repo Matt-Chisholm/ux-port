4a1e6cdc06267aa0c7c810b693aa60040072c69b
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-04 10:24:03 -0800

    new button styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,17 @@ import "../styles/Nav.css";
 export default function Nav({ setPage }) {
   return (
     <div className='nav-container'>
-      <button className='nav-button' onClick={() => setPage("home")}>
-        Home
+      <button className='button-48' onClick={() => setPage("home")}>
+        <span className='text'>Home</span>
       </button>
-      <button className='nav-button' onClick={() => setPage("projects")}>
-        Projects
+      <button className='button-48' onClick={() => setPage("projects")}>
+        <span className='text'>Projects</span>
       </button>
-      <button className='nav-button' onClick={() => setPage("about")}>
-        About
+      <button className='button-48' onClick={() => setPage("about")}>
+        <span className='text'>About</span>
       </button>
-      <button className='nav-button' onClick={() => setPage("contact")}>
-        Contact
+      <button className='button-48' onClick={() => setPage("contact")}>
+        <span className='text'>Contact</span>
       </button>
     </div>
   );
