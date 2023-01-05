c1f7474df03befd3dbde4bee5b7be66ea57e2919
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-05 11:29:08 -0800

    paw5 img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,13 +25,25 @@ export default function Projects() {
           </p>
         </div>
         <div className='project'>
-          <h2 className='project-title'>Project 3</h2>
+          <h2 className='project-title'>Paw-Five</h2>
+          <img
+            src='https://github.com/Matt-Chisholm/Paw-Five/raw/master/docs/readme.md/home.gif?raw=true'
+            alt='Paw-Five
+            Demo'
+            className='project-image'
+          />
           <p className='project-description'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            voluptates, quod, quia, quae voluptatibus voluptatem quidem
-            necessitatibus voluptas quibusdam quos nesciunt. Quisquam, quae
-            voluptates. Quisquam, quae voluptates. Quisquam, quae voluptates.
+            PAWFIVE is a specialized training app for dogs, supported by their
+            owners, to track their skill progressions and sessions, while aided
+            by voice recognition features. The app is built with React, Express,
+            Bootstrap, Axios and a voice recognition API called Wit.ai.
           </p>
+          <a
+            href='https://github.com/Matt-Chisholm/Paw-Five'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <button className='project-button'>Github</button>
+          </a>
         </div>
       </div>
     </div>
