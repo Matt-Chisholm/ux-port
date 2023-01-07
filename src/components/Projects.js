b48e11b6db450eee32f157b8e858ce859eb3eeb3
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-07 13:56:07 -0800

    more responsive tweaks

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Projects.css";
+import Chat from "../assets/chatbot.jpg";
 
 export default function Projects() {
   return (
@@ -27,11 +28,7 @@ export default function Projects() {
         </div>
         <div className='project'>
           <h2 className='project-title'>React Chatbot</h2>
-          <img
-            src='https://attachment.outlook.live.net/owa/MSA%3Amathesonchisholm%40hotmail.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADAwATYwMAItY2IyZC00OWU0LTAwAi0wMAoARgAAA213QDRe51BKq13MP2RhVbQHAJV78GaXYNNBi%2BYWm7g6kXIAAAIBDAAAAJV78GaXYNNBi%2BYWm7g6kXIABva22GMAAAABEgAQAKXqwngFr1pFqEX8l5l8iZM%3D&thumbnailType=2&isc=1&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IkQ4OThGN0RDMjk2ODQ1MDk1RUUwREZGQ0MzODBBOTM5NjUwNDNFNjQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiIySmozM0Nsb1JRbGU0Tl84dzRDcE9XVUVQbVEifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2subGl2ZS5jb20iLCJ1YyI6IjM4MDllN2U0YmIwNjQyMDQ4ZDQ1NzIwMDkwNDUwNDU2IiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJpc3NyaW5nIjoiU0lQIiwiYXBwY3R4Ijoie1wibXNleGNocHJvdFwiOlwib3dhXCIsXCJwdWlkXCI6XCIxNjg4ODUzMjY5MDA2ODIwXCIsXCJzY29wZVwiOlwiT3dhRG93bmxvYWRcIixcIm9pZFwiOlwiMDAwNjAwMDAtY2IyZC00OWU0LTAwMDAtMDAwMDAwMDAwMDAwXCIsXCJwcmltYXJ5c2lkXCI6XCJTLTEtMjgyNy0zOTMyMTYtMzQwODc0Mjg4NFwifSIsIm5iZiI6MTY3Mjk0ODI5NywiZXhwIjoxNjcyOTQ4ODk3LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAODRkZjllN2YtZTlmNi00MGFmLWI0MzUtYWFhYWFhYWFhYWFhIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnQub3V0bG9vay5saXZlLm5ldEA4NGRmOWU3Zi1lOWY2LTQwYWYtYjQzNS1hYWFhYWFhYWFhYWEiLCJoYXBwIjoib3dhIn0.o9i9C9Bjh8u9yeDfElLxy2ooywjqgTm44acbZMJdReFsgbLzLzgLIwQdTrWgNRfj8Z9T_dGxgNg6cJ6UUJjvFqf62QPLkLOegQwrM2Q3IqHKcLMI2lNMmLj_MXq8Wi5KUQA3YRo1LKxz2Vv3aPT2lxRv33bkeBJbz2BzQJ3mwA2NWdJwXgGOSdps7fjMin9iSF75B3NNyklomb-gM6db40wEWn-kpgd1nawtpI6i6lphQ9fw7e1lb3Y3K8ymU2aw8YM9foom3rYAg0m2ArSBY6QQhpk7ZliDW7t_u8vlUJtAa90j7eLcUfBw5IgX_8h7G0HSnXOd7evSPRCvHRImVA&X-OWA-CANARY=IGkVEUm-6Ui2zF6Cb7tQpSBHOVdW79oYT6W9l9__VACNLvxCpNPs-76hY86M062nzJpQZgLsSJM.&owa=outlook.live.com&scriptVer=20221209009.13&animation=true'
-            alt='Paw-Five Demo'
-            className='project-image'
-          />
+          <img src={Chat} alt='Chatbot Pic' className='project-image' />
           <p className='project-description'>
             A fun chatbot, built with React, that can answer questions about
             relevant coding topics. (Non-ML bot)
